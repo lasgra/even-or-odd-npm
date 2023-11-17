c75ae115c51760b85f9bbeb89418f8eaa021e66a
Tomek <tomekoclon1@gmail.com>
2023-11-17 20:37:59 +0100

    changes

---
+++ index.js
@@ -0,0 +1,28 @@
+function IsEvenOrOdd(number){
+    if (typeof number === 'number') {
+        if (number === 0){return "even"}
+        if (number === 1){return "odd"}
+        if (number === 2){return "even"}
+        if (number === 3){return "odd"}
+        if (number === 4){return "even"}
+        if (number === 5){return "odd"}
+        if (number === 6){return "even"}
+        if (number === 7){return "odd"}
+        if (number === 8){return "even"}
+        if (number === 9){return "odd"}
+        if (number === 10){return "even"}
+        if (number === 11){return "odd"}
+        if (number === 12){return "even"}
+        if (number === 13){return "odd"}
+        if (number === 14){return "even"}
+        if (number === 15){return "odd"}
+        if (number === 16){return "even"}
+        if (number === 17){return "odd"}
+        if (number === 18){return "even"}
+        if (number === 19){return "odd"}
+        if (number === 20){return "even"}
+    }
+    return false
+}
+
+module.exports = IsEvenOrOdd
